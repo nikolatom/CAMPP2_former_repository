8b4f49d57305df66eb65af3ae8eea1af94315b48
nikolatom <1nikola.tom@gmail.com>
2022-04-21 14:04:54 +0200

    changes in impute.knn part
    Former-commit-id: 8dbff618c153e4583a90bb03364f64a280d33149 [formerly 9065ac88934bac87906211be5310ad2f2420e8f1]
    Former-commit-id: a3f89ba5e5b68f4b97d5bc1256c62d5bdba158d9
    Former-commit-id: df6863fde1532755bef4ddce7dff26d65bd91082
    Former-commit-id: d37ba606eb542ea976c034c81db45a119cd2fdb8

--- R/Replace_NAs.R
+++ R/Replace_NAs.R
@@ -12,7 +12,7 @@
 
 ReplaceNAs <- function(data) {
     na_row <- apply(data, 1, function(x) (sum(is.na(x))/ncol(data))*100)
-    cat(paste0(" The input data has between " , round(min(na_row), digits = 2), "% - ", round(max(na_row), digits = 2),"%", " missing values per row. Samples (rows) with more than 70% missing values will be removed. \n"))
+    cat(paste0(" The input data has between " , round(min(na_row), digits = 2), "% - ", round(max(na_row), digits = 2),"%", " missing (NA) values per row. Samples (rows) with more than 70% missing values will be removed. \n"))
     removeNA=NULL
     removeNA <- which(as.vector(na_row) > 70)
     if(length(removeNA)>0){
@@ -20,7 +20,7 @@ ReplaceNAs <- function(data) {
     }
     
     na_col <- apply(data, 2, function(x) (sum(is.na(x))/nrow(data))*100)
-    cat(paste0(" The input data has between " , round(min(na_col), digits = 2), "% - ", round(max(na_col), digits = 2),"%", " missing values per column. Samples (rows) with more than 80% missing values will be removed. \n N.B. Uncertainty increases with number of missing values! \n "))
+    cat(paste0(" The input data has between " , round(min(na_col), digits = 2), "% - ", round(max(na_col), digits = 2),"%", " missing (NA) values per column. Samples (rows) with more than 80% missing values will be removed. \n N.B. Uncertainty increases with number of missing values! \n "))
     removeNA=NULL
     removeNA <- which(as.vector(na_col) > 80)
     if(length(removeNA)>0){
@@ -30,19 +30,18 @@ ReplaceNAs <- function(data) {
     still.NA <- c(unique(as.vector(is.na(data))))
     if (TRUE %in% still.NA) {
         varnames <- rownames(data)
-        ###NOT SURE ABOUT THIS PART, I think it wasn't working correctly. I have added 1 line below
-        # if (checkData(as.matrix(my.data))[1] == FALSE) {
-        #     my.data <- as.data.frame(lapply(my.data, as.numeric))
-        # }
         
-        data<-as.data.frame(lapply(data, as.numeric))
+        if (checkData(as.matrix(data))[1] == FALSE) {
+            data <- as.data.frame(lapply(data, as.numeric))
+        }
+
         data.lls=NULL
         print("Running Missing value estimation using local least squares (llsImpute)")
         file <- try(data.lls <- data.frame(completeObs(llsImpute(as.matrix(data), k = 10, correlation="spearman", allVariables=TRUE))), silent =TRUE)
         
         hasNegB <- unique(as.vector(data < 0))
         hasNegA <- unique(as.vector(data.lls < 0))
-        if (class(file) == "try-error" || TRUE %in% hasNegA & hasNegB == FALSE) {       ##If the 1st approach not working, try impute.knn)
+        if (class(file) == "try-error" || TRUE %in% hasNegA & FALSE %in% hasNegB) {       ##If the 1st approach is not working, CAMPP2 tries impute.knn)
             print("The first round of missing values imputation failed or resulted in negative values. Running additional missing value imputation using impute.knn. Rows with more than 50% missing values will be imputed using the overall mean per sample.")
             data <- impute.knn(as.matrix(data), rowmax = 0.5)                           ##This might generate "Error: C stack usage  xxx is too close to the limit"
             data <- data.frame(data$data)
