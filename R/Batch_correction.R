a07ff73356f37388c993dab7927c64a7a0940b62
nikolatom <1nikola.tom@gmail.com>
2022-04-22 17:01:55 +0200

    minor changes in error printing and parameter's description
    Former-commit-id: 5a10d66dd86eafcb0e54ce5bad2b2696edd97790 [formerly 06f08c1d02073f1d09147d3c55a9bb395e719bd6]
    Former-commit-id: a65617bba1567072607aaaf360b9f11e4965339b
    Former-commit-id: 388891ba96c2d43d8f552a24a7a2789a13a6a05c
    Former-commit-id: f65ddb1f3f146cbfe8f9eb0eb2992c1a7d188cd0

--- R/Batch_correction.R
+++ R/Batch_correction.R
@@ -1,9 +1,9 @@
 #' @title Batch correction
-#' @description Function for a batch correction. Batch corrected data are NOT for DEA analysis with limma.
-#' @param data a feature counts matrix (Elist format)
+#' @description Function for a batch correction. Batch corrected data are NOT intended for a DEA analysis with limma.
+#' @param data Elist (seq technology) or array (array, ms, other technologies) of normalized, filtered and transformed (gene) counts data
 #' @param databatch TRUE/FALSE value indicating activation/deactivation of batch correction. Boolean value is automatically set based on the definition of the "batches" parameter.
 #' @param batch a factor derived from metadata column including information about a batch for each sample from data
-#' @param group a factor derived from metadata column selected as a sample group (eg. diagnosis)
+#' @param group a factor derived from metadata column selected as a sample group (e.g. diagnosis)
 #' @param technology a string vector of length 1 (or two in case of 2 datasets) defining technology used for generating the data. Allowed types are: 'array', 'seq', 'ms' or 'other'.
 #' @import sva
 #' @export
@@ -16,7 +16,10 @@
 BatchCorrect<-function(data,batch,databatch=FALSE,group,technology){
     
     data.batch=NULL
-    if (databatch == TRUE){
+    if (ncol(data)!=length(batch)) {
+        stop("Batch correction selected BUT number of samples and length of provided batch metadata don't fit")
+    }
+    else if (databatch == TRUE){
         if (length(batch) > 0) {
             design <-  model.matrix(~group)
 
@@ -25,13 +28,6 @@ BatchCorrect<-function(data,batch,databatch=FALSE,group,technology){
             } else {
                 data.batch <- ComBat(as.matrix(data), batch, design, par.prior=TRUE,prior.plots=FALSE)
             }
-
-        }
-        if (is.null(batch)) {
-            print("Batch correction selected BUT 'batch' parameter remains NULL ")
-        }
-        if (ncol(data)!=length(batch)) {
-            print("Batch correction selected BUT numbers of samples and provided batch metadata don't fit")
         }
     }
 
