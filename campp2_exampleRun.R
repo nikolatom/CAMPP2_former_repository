5760b83fe743f06fa93d77b501b2d691399a5ed5
nikolatom <1nikola.tom@gmail.com>
2022-04-26 15:22:51 +0200

    updated example script
    Former-commit-id: 300d9f8cb8337cff60bbd57dc277b9f7647b3eb2 [formerly 755ea276e35971ed2ccc20f8b8f08b638b319d77]
    Former-commit-id: 3e6fcfc1b299e0208dab2809309f6c97825282d2
    Former-commit-id: 789950ff39d19efb97f8c5c085963b454d443497
    Former-commit-id: 9484162595124296c654f53a9031a743d56ed9d4

--- campp2_exampleRun.R
+++ campp2_exampleRun.R
@@ -5,7 +5,7 @@ library(pROC)
 library(VennDiagram)
 
 getwd()
-setwd("/Users/nikto/opt/campp_bioconducor/CAMPP2/data/BRCA")
+setwd("../data_nik")
 
 
 
@@ -50,6 +50,6 @@ dataset3<-cbind(dataset1,dataset2)
 metadata3<-rbind(metadata1,metadata2)
 
 
-runCampp2(WGCNA="DE", signif=c(1,1,1,1),survival="DE",plot.heatmap="DE",batches=c("tumor_stage","tumor_stage"),kmeans=TRUE, plot.mds=TRUE,prefix="test_dual_surv_covar7", data1=dataset1, data2=dataset2, metadata1=metadata1,metadata2=metadata2, groups=c("IDs", "diagnosis","IDs", "diagnosis"), technology=c("seq","seq"))
+runCampp2(batches=c("tumor_stage","tumor_stage"),prefix="test_replace_NA", data1=dataset1, data2=dataset2, metadata1=metadata1,metadata2=metadata2, groups=c("IDs", "diagnosis","IDs", "diagnosis"), technology=c("seq","seq"))
 
 
