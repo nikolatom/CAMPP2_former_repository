ce351391e3c1991acdd0d5124798b2c935204a50
nikolatom <1nikola.tom@gmail.com>
2022-05-02 18:30:50 +0200

    fixes based on Matteo's review
    Former-commit-id: f57772dbfd440fdd597dd2d01400f6292e63c925 [formerly 68ae3069e15c48fd8a9c604411899e7dfc1a06da]
    Former-commit-id: d61824cbde1cfe40fac64f60a1ad11cc568bc731
    Former-commit-id: 04a738a1d9ea47cf2346788a1185b62f7ff149e3
    Former-commit-id: 9d0f507df7aa0633bfadc352f07698ae68fcde0b

--- R/Batch_correction.R
+++ R/Batch_correction.R
@@ -1,10 +1,9 @@
 #' @title Batch correction
 #' @description Function for a batch correction. Batch corrected data are NOT intended for a DEA analysis with limma.
 #' @param data Elist (seq technology) or array (array, ms, other technologies) of normalized, filtered and transformed (gene) counts data
-#' @param databatch TRUE/FALSE value indicating activation/deactivation of batch correction. Boolean value is automatically set based on the definition of the "batches" parameter.
 #' @param batch a factor derived from metadata column including information about a batch for each sample from data
 #' @param group a factor derived from metadata column selected as a sample group (e.g. diagnosis)
-#' @param technology a string vector of length 1 (or two in case of 2 datasets) defining technology used for generating the data. Allowed types are: 'array', 'seq', 'ms' or 'other'.
+#' @param technology a string vector of length 1 defining technology used for generating the data. Allowed types are: 'array', 'seq', 'ms' or 'other'.
 #' @import sva
 #' @export
 #' @seealso
@@ -13,23 +12,25 @@
 #' ...
 #' }
 
-BatchCorrect<-function(data,batch,databatch=FALSE,group,technology){
+BatchCorrect<-function(data,batch,group,technology){
     
+    # section in which we define correctness of input parameters
+    if (!(technology) %in% c("seq", "array", "ms", "other")) {
+        stop("Defined technology is not supported.")
+    }
+        
     data.batch=NULL
     if (ncol(data)!=length(batch)) {
         stop("Batch correction selected BUT number of samples and length of provided batch metadata don't fit")
     }
-    else if (databatch == TRUE){
-        if (length(batch) > 0) {
-            design <-  model.matrix(~group)
-
-            if (technology == "seq") {
-                data.batch <- ComBat(as.matrix(data$E), batch, design, par.prior=TRUE,prior.plots=FALSE)
-            } else {
-                data.batch <- ComBat(as.matrix(data), batch, design, par.prior=TRUE,prior.plots=FALSE)
+    else if (length(batch) > 0) {
+        design <-  model.matrix(~group)
+        if (technology == "seq") {
+            data.batch <- ComBat(as.matrix(data$E), batch, design, par.prior=TRUE,prior.plots=FALSE)
+        } else {
+            data.batch <- ComBat(as.matrix(data), batch, design, par.prior=TRUE,prior.plots=FALSE)
             }
-        }
     }
 
-    return(list("data.batch"=data.batch))
+    return(data.batch)
 }
