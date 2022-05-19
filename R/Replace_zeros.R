f9726088e27a15163695392810386bd41183ec98
nikolatom <1nikola.tom@gmail.com>
2022-05-19 20:12:57 +0200

    implementation of checking for zeros and negative counts
    Former-commit-id: 9b4b4d235533d93a361789a9cfadfff3cd22033b [formerly a69939b5757daee3fd54399f27af4977e0e67b47]
    Former-commit-id: 6623dabe269c20dde16ee493384d0c9d2b708872
    Former-commit-id: b9498f244576fbb773ba78044ded3e1e7d45d9c1
    Former-commit-id: 47b8a87a2464dbfca9c1773e9a9ed39fb74dddef

--- R/Replace_zeros.R
+++ R/Replace_zeros.R
@@ -1,29 +1,46 @@
 #' @title Replace zeros
-#' @description Replacing zero values in the data (counts).
+#' @description A function for a detection of zero/negative values and for replacing zero values in the data.
 #' @param data a dataframe of gene/abundance counts.
 #' @param group a factor specifying group which should be represented by a column from a metadata file.
 #' @export
 #' @import impute
 #' @seealso
-#' @return a data frame with replaced zeros
+#' @return a data frame with replaced zeros and a dataframe with original data
 #' @examples \dontrun{
 #' ...
 #' }
 
 
-ReplaceZero <- function(data, group) {
+ReplaceZeros <- function(data, group) {
+    hasZero <- unique(as.vector(data == 0))
+    if(TRUE %in% hasZero){
+        print("data includes 0-value(s)")
+    }else{
+        print("data doesn't include 0-value(s)")
+    }
+
+    hasNeg <- unique(as.vector(data < 0))
+    if(TRUE %in% hasNeg){
+        print("data includes negative value(s)")
+    }else{
+        print("data doesn't include negative value(s)")
+    }
+
 
     smallestGr <- min(as.numeric(table(group))) # a size of the smallest group of samples
-    greaterthanBG <- apply(data, 1, function(x) sum(x > 0)) #rows with sum>0
-    lessthanBG  <- which(as.numeric(greaterthanBG) < smallestGr)
+    greaterthanBG <- apply(data, 1, function(x) sum(x > 0)) #counting non-zero counts for each feature
+    lessthanBG  <- which(as.numeric(greaterthanBG) < smallestGr) #features having number of zero counts higher than the size of the smallest sample group.
 
+    data.original<-NULL
     if (length(lessthanBG) > 0) {
+        data.original<-data
         data <- data[-lessthanBG,] #removal of lines with low counts
+        print(paste0(length(lessthanBG)," features will be removed because of the low gene counts"))
     }
 
     min_per_row <- as.vector(apply(data, 1, function(x) min(x[x != 0])))
     for(i in 1:nrow(data)){
         data[i, data[i,] == 0] <- min_per_row[i] #substitution of zeros with min values per row
     }
-    return(data)
+    return(list(data, data.original))
 }
