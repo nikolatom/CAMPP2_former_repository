9889c21f904107c26dbf7ab76d57c935ebcffd4c
Mathilde Bechgaard <mathilde@bechgaard.dk>
2022-05-19 11:02:51 +0200

    Added function for transforming gene names with biomaRt
    Former-commit-id: d2a0499e5df2ad4cb311ab374b9011b478ef7ae2 [formerly 079d7be8a1099c0fd752b58ada4e5fccf8dd34cd]
    Former-commit-id: e482f12e9782e1f0ac208c3b8a9fbcafd3e1214c
    Former-commit-id: 3727a71b3348d95f76c8363283cb6730298d3fc5
    Former-commit-id: 6b9388cf85ac27769de17bbd01d7fd4f9e25f9fa

---
+++ R/AddGeneName.R
@@ -0,0 +1,31 @@
+#' @title Add Gene Name function
+#' @description A function for transforming stable gene IDs into HUGO IDs using biomaRt.
+#' @param data a data matrix containing a column of stable gene IDs.
+#' @export
+#' @import biomaRt
+#' @seealso
+#' @return the input data matrix merged with a gene name column.
+#' @examples \dontrun{
+#' ...
+#' }
+
+AddGeneName <- function(data) {
+
+    ensembl104 <- useEnsembl(biomart = 'genes', dataset = 'hsapiens_gene_ensembl',version = 104)
+
+    ensembl_id <- as.data.frame(data$name)
+    colnames(ensembl_id) <- "Gene stable ID"
+
+    gene_names <- getBM(attributes = c('ensembl_gene_id', 'external_gene_name'),
+                        mart = ensembl104,
+                        values = ensembl_id,
+                        filters = 'ensembl_gene_id',
+                        uniqueRows = TRUE,
+                        bmHeader = T)
+
+    names(data)[names(data) == 'name'] <- "Gene stable ID"
+    data_merged <- merge(gene_names, data, by="Gene stable ID", all = TRUE)
+
+    return(data_merged)
+    #write.table(data_merged,file="withGeneNames")
+}
