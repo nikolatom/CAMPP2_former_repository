a09f3c8f652e9d92c3e677cdd53ee0cd34c103e4
Mathilde Bechgaard <mathilde@bechgaard.dk>
2022-05-20 10:25:54 +0200

    Adding visualizations to DEA analysis
    Former-commit-id: cc654c42784b45add42ad1557626c1e84eae118d [formerly 31de20ea1e58badf2fdc3dc58ece56b505e38d76]
    Former-commit-id: 58dab6f9c590d48182d5006e21571d9eb7486939
    Former-commit-id: 72843e1041d8632e7465b180c85de403633f5c00
    Former-commit-id: 7bd1265b1d6137befb0b7ef6eea56d61773d7212

---
+++ R/VolcanoPlot.R
@@ -0,0 +1,34 @@
+#' @title Make Volcano Plot
+#' @description A function for making volcano plots from DEA results.
+#' @param data a data matrix from RunDEA containing genes, AveExpr, logFCs,p.value, adj.p.value ect.
+#' @export
+#' @import ggplot2
+#' @import ggrepel
+#' @seealso
+#' @return a volcano plot showcasing significantly differentially expressed genes.
+#' @examples \dontrun{
+#' ...
+#' }
+
+MakeVolcano <- function(data) {
+
+    data <- data[order(data$adj.P.Val),]
+
+    # add a column of NAs
+    data$diffexpressed <- "NO"
+    # Get all significantly differentially expressed genes
+    data$diffexpressed[data$logFC > 1.4 & data$adj.P.Val < 0.05] <- "UP"
+    data$diffexpressed[data$logFC < -1.4 & data$adj.P.Val < 0.05] <- "DOWN"
+
+    # Create a new column "delabel" to de, that will contain the name of genes differentially expressed (NA in case they are not)
+    data$DEAlabel <- NA
+    data[1:10,]$DEAlabel[data[1:10,]$diffexpressed != "NO"] <- data[1:10,]$"Gene name"[data[1:10,]$diffexpressed != "NO"]
+
+    ggplot(data, aes(x=logFC, y=-log10(adj.P.Val), col=data$diffexpressed, label=data$DEAlabel)) +
+        geom_point() +
+        theme_minimal() +
+        geom_text_repel() +
+        scale_color_manual(values=c("blue", "black", "red")) +
+        geom_vline(xintercept=c(-1.4, 1.4), col="black") +
+        geom_hline(yintercept=-log10(0.05), col="black")
+}
