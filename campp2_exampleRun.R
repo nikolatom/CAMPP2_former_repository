41310ffbe27e31b857425f661a1cc9d1c4f2e97a
nikolatom <1nikola.tom@gmail.com>
2022-06-20 16:18:06 +0200

    minor change
    Former-commit-id: 5cb23c5bb6c26e6d0f4e9cfab1660c1a24f97212 [formerly 65cd35ccba4cb2e25accb343e66093e5a55aa5cc]
    Former-commit-id: 338d7ded6dfc369bc8c38c1193cd9fa01a30f5c7
    Former-commit-id: 7185a0b5cf636d67200bf8b28ded2fa23b6a6438
    Former-commit-id: cac8e7da98566ffb74417996cf611ea378649645

--- campp2_exampleRun.R
+++ campp2_exampleRun.R
@@ -9,7 +9,7 @@ library(dynamicTreeCut)
 ###This command will run CAMPP2 with default settigs using example data which are included in the package
 runCampp2(batches=c("tumor_stage","tumor_stage"),prefix="test_CAMPP2", data1=dataset1, data2=dataset2, metadata1=metadata1,metadata2=metadata2, groups=c("IDs", "diagnosis","IDs", "diagnosis"), technology=c("seq","seq"))
 
-###Code for checking zeros will be added when finished
+###Code for checking zeros and negative values
 zerofix<-FixZeros(dataset1,metadata1$diagnosis)
 
 ###Code for testing the function for Replacing NAs
