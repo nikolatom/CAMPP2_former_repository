01c22ba5c316c6cad1035cfc2c257c3548b4c7e9
Nikola Tom <1nikola.tom@gmail.com>
2022-09-07 16:03:31 +0200

    fix after 1st revision
    Former-commit-id: 95c5b43bca6c379dc3be2017435fa3642af75a84 [formerly 8529fa83ea10ee60517951ba894dfd03193d5fd6]
    Former-commit-id: e395015f77c472acd8c7058af616cffa0ce23548
    Former-commit-id: 0a400fa4046cf56efabb3289ff85d07cd34bf023
    Former-commit-id: c4fd60077fa8731ffc46eba494dc8a34a691686f

--- R/Estimate_K_means.R
+++ R/Estimate_K_means.R
@@ -7,20 +7,21 @@
 #' @export
 #' @import mclust
 #' @seealso
-#' @return a data frame with a number of clusters
+#' @return a list including:
+#' 1) a data frame with a number of clusters corresponding to G from BIC object.
+#' G represents a number of mixture components in the model corresponding to
+#' the optimal BIC
+#' 2) an 'mclustBIC' object, which is the result of applying mclustBIC to data
 #' @examples \dontrun{
 #' EstimateKmeans(t(campp2_brca_1_batchCorrected[1:2000,]))
 #' }
 
 EstimateKmeans <- function(data) {
     BIC <- mclustBIC(data)
-    plot(BIC)
     mod1 <- Mclust(data, x = BIC) #obtained model
-    # fviz_nbclust(my_data, kmeans, method = "silhouette") #alternative approach
     num.km.clusters <- as.numeric(mod1$G)  #number of clusters according to the best model
     print(paste0("number of clusters according to the best model, G: ",num.km.clusters))
     summary.clust<-summary(mod1, parameters = FALSE) #summary for the best model
     print(summary.clust)
-    return(num.km.clusters)
-
+    return(list("num.km.clusters"=num.km.clusters, "BIC"=BIC))
 }
