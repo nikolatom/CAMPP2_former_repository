e1a5104aaab47ae95a50b229cf8986902d605086
nikolatom <1nikola.tom@gmail.com>
2022-10-31 15:57:30 +0100

    add put back example data
    Former-commit-id: 8b0bbd00d0f3836a0b7991b19ea2c54ee332c909 [formerly 12027582ea356859e86581833aa2bff7c4dcf714]
    Former-commit-id: e7323c47ea9559b7bda058742c4dd7de1da8fc9f
    Former-commit-id: ca072a8ef7bd18dba972c83ca7480a2df5c70d17
    Former-commit-id: 1c9366e33c3f39fcf0eb671b6cd82e272e6594a5
    Former-commit-id: 2a908cb17d627e3efe5c4b414bdf6d431c1d4c07

---
+++ campp2_exampleRun.R
@@ -0,0 +1,32 @@
+library(CAMPP2)
+library(edgeR)
+library(sva)
+library(pROC)
+library(VennDiagram)
+library(rms)
+library(dynamicTreeCut)
+
+###This command will run CAMPP2 with default settigs using example data which are included in the package
+runCampp2(batches=c("tumor_stage","tumor_stage"),prefix="test_CAMPP2", data1=campp2_brca_1, data2=campp2_brca_2, metadata1=campp2_brca_1_meta, 
+metadata2=campp2_brca_2_meta, groups=c("IDs", "diagnosis","IDs", "diagnosis"), technology=c("seq","seq"))
+
+###An example code for testing the function for Replacing NAs
+n<-10000
+Na_campp2_brca_1<-apply (campp2_brca_1, 2, function(x) {x[sample( c(1:n), floor(n/10))] <- NA; x} )
+replacedNAs<-ReplaceNAs(data=Na_campp2_brca_1)
+
+###An example code for fixing zeros and negative values
+zerofix<-FixZeros(data=replacedNAs,group=campp2_brca_1_meta$diagnosis, remove.sparse.features=TRUE)
+
+###An example code for normalization
+normalized_data<-NormalizeData(data=zerofix,group=campp2_brca_1_meta$diagnosis,standardize="TMM",transform="voom",technology="seq")
+
+###A example code for batch correction
+batch_corrected_data<-BatchCorrect(normalized_data,campp2_brca_1_meta$tumor_stage,campp2_brca_1_meta$diagnosis,technology="seq")
+
+###An example code for running differential gene expression analysis
+runCampp2(prefix="Testing_DEA", data1=campp2_brca_1, metadata1=campp2_brca_1_meta, groups=c("IDs", "diagnosis"), technology=c("seq"), 
+block=campp2_brca_1_meta$tumor_stage)
+
+###An example code for running differential expression analysis with visualizations
+runCampp2(prefix="Testing_DEA_visuals", data1=campp2_brca_1, metadata1=campp
